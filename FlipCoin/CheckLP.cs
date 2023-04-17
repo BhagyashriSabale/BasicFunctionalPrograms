65b6c76ea72724a124fc9ffe854099de26978515
BhagyashriSabale <balisabale16@gmail.com>
2023-04-17 09:49:50 +0530

    [Bhagyashri] Flip a coin and print the percentage

---
+++ FlipCoin/CheckLP.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionPrograms.FlipCoin
+{
+    internal class CheckLP
+    {
+        public void FlipCoin()
+        {
+            //Take user input
+            Console.WriteLine("Enter the number of times to flip the coin:");
+
+            //Check random no is less than 0.5  
+            int numFlips = 0;
+            while (numFlips <= 0)
+            {
+                string input = Console.ReadLine();
+                if (!int.TryParse(input, out numFlips) || numFlips <= 0)
+                {
+                    Console.WriteLine("Invalid input, please enter a positive integer.");
+                }
+            }
+
+
+            //Print percentage head and tail
+            int numHeads = 0;
+            int numTails = 0;
+            Random rand = new Random();
+            for (int i = 0; i < numFlips; i++)
+            {
+                if (rand.NextDouble() < 0.5)
+                {
+                    numTails++;
+                }
+                else
+                {
+                    numHeads++;
+                }
+
+            }
+            double percentHeads = ((double)numHeads / numFlips) * 100;
+            double percentTails = ((double)numTails / numFlips) * 100;
+
+            Console.WriteLine("Percentage of heads: " + percentHeads);
+            Console.WriteLine("Percentage of tails: " + percentTails);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace BasicFunctionPrograms
+﻿using BasicFunctionPrograms.FlipCoin;
+
+namespace BasicFunctionPrograms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Functional Programs");
+
+            Console.WriteLine("Please choose a 1 program from below List");
+            Console.WriteLine("1.FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option) 
+            {
+                case 1:
+                    CheckLP checkLP = new CheckLP();
+                    checkLP.FlipCoin();
+                    break;
+            
+            }
         }
     }
 }
